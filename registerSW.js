93416bd7799531655dc4cfdc3ecb9088fd31bb2b
Projectku <minotaurmoon@gmail.com>
2025-12-25 16:50:26 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/NAMA-REPO/sw.js', { scope: '/NAMA-REPO/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/portofoliobii/sw.js', { scope: '/portofoliobii/' })})}
